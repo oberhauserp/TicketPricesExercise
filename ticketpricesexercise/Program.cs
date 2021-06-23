f9efeafa39040bfd158a1eddd68ad62038f98c2d
Oberhauser <34341@nelnet.net>
2021-06-23 10:44:43 -0500

    base program complete

---
+++ ticketpricesexercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TicketPricesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = 0;
+            bool isStudent = false; 
+            Console.WriteLine("How old are you?");
+            int age =  Convert.ToInt32(Console.ReadLine());
+            Console.Write("Are you a student?");
+            string StudentString = Console.ReadLine();
+
+            if(StudentString == "Y")
+            {
+                isStudent = true;
+            }
+
+            if(age >=  65 && isStudent == false)
+            {
+                price = 10;
+            }
+            else if(age >= 13 && age <= 64 && isStudent == false)
+            {
+                price = 14;
+            }
+            else
+            {
+                price = 8;
+            }
+
+            Console.WriteLine("your ticket price is " + price);
+        }
+    }
+}
