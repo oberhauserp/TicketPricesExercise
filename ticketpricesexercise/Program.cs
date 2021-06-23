6f525e88251c376b9f0ac6c7a2a00157647c9cd2
Oberhauser <34341@nelnet.net>
2021-06-23 11:18:52 -0500

    added try catch

--- ticketpricesexercise/Program.cs
+++ ticketpricesexercise/Program.cs
@@ -8,14 +8,26 @@ namespace TicketPricesExercise
         {
             int price = 0;
             bool isStudent = false; 
+            
             Console.WriteLine("How old are you?");
             int age =  Convert.ToInt32(Console.ReadLine());
             Console.Write("Are you a student?");
             string StudentString = Console.ReadLine();
-
-            if(StudentString == "Y")
+            
+            
+            if(StudentString == "Y" || StudentString == "y")
             {
                 isStudent = true;
+            }
+
+            try
+            {
+                ProcessAge(age);
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Error: Number cannot be negative!");
+                System.Environment.Exit(1);
             }
 
             if(age >=  65 && isStudent == false)
@@ -31,7 +43,19 @@ namespace TicketPricesExercise
                 price = 8;
             }
 
+
+
             Console.WriteLine("your ticket price is " + price);
         }
+
+    static void ProcessAge(int age)
+    {
+        if(age < 0)
+        {
+            throw new ArgumentException("Age must not be a negative number");
+        }
+    }
+
+
     }
 }
